9b1486429d1295cccd449b819d8ff826e6a21e82
algrel <alinagrel2006@gmail.com>
2025-12-05 02:24:03 +0300

    Create bd_structure.py
    Bd_structure

---
+++ database/bd_structure.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, BigInteger, Boolean, String
+from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy.orm import relationship
+Base = declarative_base()
+
+class Chat(Base):
+    __tablename__ = 'chats'
+    
+    id = Column(BigInteger, primary_key=True)  # Может быть стоит Integer
+    paused = Column(Boolean, default=True, nullable=False)
+    # whitelist_entries = relationship("Whitelist", back_populates="chat")
+
+
+class Whitelist(Base):
+    __tablename__ = 'whitelist'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    chat_id = Column(BigInteger, nullable=False)  # тоже самое про Integer
+    username = Column(String(100), nullable=False)  # username вместо userId
+    #chat = relationship("Chat", back_populates="whitelist_entries")
