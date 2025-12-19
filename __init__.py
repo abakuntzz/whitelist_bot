f152c5eb30dd9996ee7669443884801e64d7449b
algrel <alinagrel2006@gmail.com>
2025-12-19 12:43:41 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,19 @@
-import asyncio
-from bot.bot_launch import activate
+import sys
+import os
 
-if __name__ == "__main__":
-    asyncio.run(activate())
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
+from bot.bot_launch import activate
+from bot.set_commands import initialise_commands
+__all__ = ['activate', 'initialise_commands']
+
+
+
+try:
+    from bot.bot_launch import activate
+    from bot.set_commands import initialise_commands
+    __all__ = ['activate', 'initialise_commands']
+except:
+    __all__=[]
