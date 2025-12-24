f87ae5f88cc7b3fce79ddf7f564c35cb3776106a
algrel <alinagrel2006@gmail.com>
2025-12-24 04:48:16 +0300

    Update bd_structure.py

--- database/bd_structure.py
+++ database/bd_structure.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, BigInteger, Boolean
 from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import relationship
 
 
 Base = declarative_base()
@@ -10,7 +9,6 @@ class Chat(Base):  # type: ignore
     __tablename__ = 'chats'
     id = Column(BigInteger, primary_key=True, autoincrement=False)
     paused = Column(Boolean, default=True, nullable=False)
-    # whitelist_entries = relationship("Whitelist", back_populates="chat")
 
 
 class Whitelist(Base):  # type: ignore
@@ -18,4 +16,3 @@ class Whitelist(Base):  # type: ignore
     id = Column(Integer, primary_key=True, autoincrement=True)
     chat_id = Column(BigInteger, nullable=False)
     user_id = Column(BigInteger, nullable=False)
-    # chat = relationship("Chat", back_populates="whitelist_entries")
