3eeb91b9b89a7086ab5d4ca5b7487f57987fb53c
algrel <alinagrel2006@gmail.com>
2025-12-24 05:26:00 +0300

    Update test_bot.py

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,7 +1,7 @@
 import pytest
-from unittest.mock import patch, AsyncMock, Mock, mock_open
 import sys
 import os
+from aiogram.types import BotCommandScopeAllGroupChats, BotCommandScopeDefault
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -12,3 +12,14 @@ import bot.set_commands as set_commands   # noqa: E402
 async def test_initialise_commands(mock_bot) -> None:
     await set_commands.initialise_commands(mock_bot)
     assert mock_bot.set_my_commands.call_count == 2
+
+    calls = mock_bot.set_my_commands.call_args_list
+    first_commands = calls[0][1]['commands']
+    first_scope = calls[0][1]['scope']
+    second_commands = calls[1][1]['commands']
+    second_scope = calls[1][1]['scope']
+
+    assert isinstance(first_scope, BotCommandScopeAllGroupChats)
+    assert len(first_commands) == 7
+    assert isinstance(second_scope, BotCommandScopeDefault)
+    assert len(second_commands) == 2
